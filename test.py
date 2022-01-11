ef5b22752b58adf55f499773df7f45ff54f87947
iota97 <giovi.cocco@gmail.com>
2022-01-11 17:47:27 +0100

    Fixes

--- test.py
+++ test.py
@@ -6,14 +6,13 @@ import re
 import sys
 import os
 import glob 
-import shutil
 
 def gulpease(nf):
 	testo = textract.process(nf, method='pdftotext').decode("utf-8")
 
 	parole  = len(re.findall(r'\w+', testo))
 	lettere = len(re.findall(r'\w', testo))
-	punti = len(re.findall('[.]+\s', testo))+len(re.findall('[;]+\s', testo)) - len(re.findall('[.]+\s+[.]', testo))
+	punti = len(re.findall('[.;]+\s', testo))
 
 	indiceG = 89+((300*punti)-(10*lettere))/parole
 
@@ -28,11 +27,8 @@ def compile(tex):
 
 def latexTest():
 	for tex in glob.glob("./**/*.tex", recursive = True):
-		min_gulpease = 40
 		if tex.startswith("./docs/template"):
 			continue
-		if tex.startswith("./docs/esterni"):
-			min_gulpease = 60
 
 		stat = compile(tex)
 
@@ -40,7 +36,7 @@ def latexTest():
 			print("Errore compilazione: " + tex)
 		else:
 			g = gulpease(tex[:-3]+"pdf")
-			if g < min_gulpease:
+			if g < 40:
 				print("Indice di Gulpease troppo basso per " + tex + ": " + str(g))
 
 
