ac27d7284644e94e98e07667d7e10346f59562c4
iota97 <giovi.cocco@gmail.com>
2022-01-13 19:10:23 +0100

    Node initial

---
+++ server/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import mysql from 'mysql';
+
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+ res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+
+  if (name === "admin" && password === "admin") {
+    res.render("success", {
+      username: name,
+    });
+  } else {
+    res.render("failure");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
+});
