2cf66fe8671bf17d051d4e50320f9f05f120b820
iota97 <giovi.cocco@gmail.com>
2022-01-16 18:58:21 +0100

    Auto compile all LaTeX files (#23)
    * Auto compile all LaTeX files
    
    * Introdotto ciclo for
    
    * Auto test for python
    
    * Fixes
    
    * Reset pdf to main
    
    * This too
    
    * Use python 3
    
    Co-authored-by: Elia Scandaletti <elia.scandaletti@gmail.com>

---
+++ test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import textract
+import re
+import sys
+import os
+import glob 
+
+def gulpease(nf):
+	testo = textract.process(nf, method='pdftotext').decode("utf-8")
+
+	parole  = len(re.findall(r'\w+', testo))
+	lettere = len(re.findall(r'\w', testo))
+	punti = len(re.findall('[.;]+\s', testo))
+
+	indiceG = 89+((300*punti)-(10*lettere))/parole
+
+	return indiceG
+
+
+def compile(tex):
+	cd = os.getcwd()
+	stat = os.system("cd " + os.path.dirname(tex) + "; pdflatex -interaction=batchmode -halt-on-error " + os.path.basename(tex) + " > /dev/null")
+	os.system("cd " + cd)
+	return stat
+
+def latexTest():
+	for tex in glob.glob("./**/*.tex", recursive = True):
+		if tex.startswith("./docs/template"):
+			continue
+
+		stat = compile(tex)
+
+		if stat != 0:
+			print("Errore compilazione: " + tex)
+		else:
+			g = gulpease(tex[:-3]+"pdf")
+			if g < 40:
+				print("Indice di Gulpease troppo basso per " + tex + ": " + str(g))
+
+
+def main():
+	latexTest()
+
+if __name__ == "__main__":
+    main()
