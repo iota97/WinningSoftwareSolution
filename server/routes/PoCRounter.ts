7df3bfb6a834afbd68ae48a78133f94feb8286cd
iota97 <giovi.cocco@gmail.com>
2022-02-05 10:00:47 +0100

    Add comment

--- server/routes/PoCRounter.ts
+++ server/routes/PoCRounter.ts
@@ -25,7 +25,16 @@ PoCRounter.get("/", async (req: Request, res: Response) => {
 });
 
 PoCRounter.get("/qr", async (req: Request, res: Response) => {
-    // http://192.168.0.15.sslip.io:8080/PoC/transazione
+    /* 
+       TL;DR
+       1) Metamask su android non permette di collegarsi direttamente a un server locale senza passare per un DNS
+       2) sslip.io (perchè quello indicato sulla loro documentazione ufficiale neanche esiste più da un bel po') ci fa da DNS
+       3) Ora i deep link funzionano solo se in HTTPS, Android dalla versione 7 non permette di aggiungere certificati senza ricompilare l'app
+       4) Creiamo un link HTTPS a tinyurl, che punta a sslip.io con nostro server locale in HTTP, i.e: http://192.168.0.15.sslip.io:8080/PoC/transazione
+       5) Notare come tinyurl è uno dei pochi shortener che permette parametri in GET
+       6) Creiamo il nostro bel QR così finchè non avremo un server HTTPS in produzione con dei certificati veri
+    */
+
     const qr_str = "https://metamask.app.link/dapp/" + "tinyurl.com/2p9xf7wx" + "?id=" + req.query.id;
     qr.toDataURL(qr_str, (err, src) => {
         if (err) res.send("Error occured in QR");
