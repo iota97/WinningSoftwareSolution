b7c63aebdff89f105ba8e1417aee60c457f13c48
iota97 <giovi.cocco@gmail.com>
2022-02-06 20:26:05 +0100

    Fixes

--- server/README.md
+++ server/README.md
@@ -1,4 +1,6 @@
 Configurazione: .env
 
+TINYURL: Creare tramite tinyurl un link al proprio IP locale che passa per sslip.io, ad esempio "http://192.168.0.15.sslip.io:8080/PoC/transazione" e mettere lo short URL nella configurazione
+
 npm install
 npm start
--- server/routes/PoCRounter.ts
+++ server/routes/PoCRounter.ts
@@ -57,26 +57,8 @@ PoCRounter.get("/transazione", async (req: Request, res: Response) => {
     if (err) {
       console.log(err);
     } else {
-      /*
-       TL;DR
-       1) Metamask su android non permette di collegarsi direttamente a un server locale su Android in quanto l'HTTP non è permesso (CLEAR_TEXT_NOT_PERMITTED)
-       2) Metamask ha un eccezione hardcoddata che lo permette per le connessioni provenienti da sslip.io (la documentazione dice di usare xip.io che non è sbagliato da agosto)
-       3) PR per aggiornare la documentazione: https://github.com/MetaMask/metamask-docs/pull/396
-       4) I deep link funzionano solo se in HTTPS, Android dalla versione 7 non permette di aggiungere certificati senza ricompilare l'app
-       5) Creiamo un link HTTPS a tinyurl, che punta a sslip.io con nostro server locale in HTTP, i.e: http://192.168.0.15.sslip.io:8080/PoC/transazione
-       6) Notare come tinyurl è uno dei pochi shortener che permette parametri in GET
-       7) Creiamo il nostro bel QR così finchè non avremo un server HTTPS in produzione con dei certificati veri
-       8) TODO: investigare l'utilizzo di "ngrok" per un tunneling HTTPS, ma probabilemente non ne vale la pena
-       9) In produzione sarà compito dei sistemisti configurare il traffico HTTP in uscita dal server per essere in HTTPS
-    */
-      const qr_str = "https://metamask.app.link/dapp/" + "tinyurl.com/2p9xf7wx" + "?id=" + req.query.id;
-      qr.toDataURL(qr_str, (err, src) => {
-        if (err) res.send("Error occured in QR");
-        const stato = item.status == 0 ? "Cancellato" : item.status == 1 ? "In attesa" : "Sbloccato";
-        
-        res.render("transazione", {idTransazione: req.query.id, qr_img: src, acquirente: item.client, paymentEntryId: item.paymentEntryId, status: stato});
-        
-      });   
+      const stato = item.status == 0 ? "Cancellato" : item.status == 1 ? "In attesa" : "Sbloccato";
+      res.render("transazione", {idTransazione: req.query.id, acquirente: item.client, paymentEntryId: item.paymentEntryId, status: stato});        
     }
   });
 });
@@ -94,7 +76,7 @@ PoCRounter.get("/qr", async (req: Request, res: Response) => {
        8) TODO: investigare l'utilizzo di "ngrok" per un tunneling HTTPS, ma probabilemente non ne vale la pena
        9) In produzione sarà compito dei sistemisti configurare il traffico HTTP in uscita dal server per essere in HTTPS
     */
-      const qr_str = "https://metamask.app.link/dapp/" + "tinyurl.com/2p9xf7wx" + "?id=" + req.query.id;
+      const qr_str = "https://metamask.app.link/dapp/" + "tinyurl.com/" + process.env.TINYURL + "?id=" + req.query.id;
       qr.toDataURL(qr_str, (err, src) => {
         if (err) res.send("Error occured in QR");
         res.render("QR", {qr_img:src});
