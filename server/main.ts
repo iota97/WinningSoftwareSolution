f36d1fad538075f4a611b15fdf90db28f06fccf4
galvagnimatteo <galvagni.matteo@protonmail.com>
2022-02-06 15:38:10 +0100

    aggiunto ascolto eventi in bc

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,28 @@
 import {createServer} from './routes/index'
+import Web3 from 'web3';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 createServer();
-/*
-shopContract.methods.getPaymentEntry(index).call()
-    .then(paymentEntry => {
-    
-    }
-*/
+
+const contractABI = JSON.parse('[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"paymentEntryId\",\"type\":\"uint256\"}],\"name\":\"addedPaymentEntry\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"settledPaymentId\",\"type\":\"uint256\"}],\"name\":\"paymentCancelled\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"settledPaymentId\",\"type\":\"uint256\"}],\"name\":\"paymentSettled\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"objId\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"price\",\"type\":\"uint256\"}],\"name\":\"addPaymentEntry\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"settledPaymentId\",\"type\":\"uint256\"}],\"name\":\"cancelSettledPayment\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"paymentEntryId\",\"type\":\"uint256\"}],\"name\":\"getPaymentEntry\",\"outputs\":[{\"components\":[{\"internalType\":\"address\",\"name\":\"seller\",\"type\":\"address\"},{\"internalType\":\"string\",\"name\":\"objId\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"price\",\"type\":\"uint256\"}],\"internalType\":\"struct ShopContract.PaymentEntry\",\"name\":\"\",\"type\":\"tuple\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"settledPaymentId\",\"type\":\"uint256\"}],\"name\":\"getSettledPayment\",\"outputs\":[{\"components\":[{\"internalType\":\"uint256\",\"name\":\"paymentEntryId\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"status\",\"type\":\"uint256\"},{\"internalType\":\"address\",\"name\":\"client\",\"type\":\"address\"}],\"internalType\":\"struct ShopContract.SettledPayment\",\"name\":\"\",\"type\":\"tuple\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"renounceOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"paymentEntryId\",\"type\":\"uint256\"}],\"name\":\"settlePayment\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]');
+
+const contractAddress = "0xd0788b436100B967B68e59665cB404f601ED841f";
+
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://speedy-nodes-nyc.moralis.io/' + process.env.APIKEY  + '/polygon/mumbai/ws'));
+
+const ShopContract = new web3.eth.Contract(contractABI, contractAddress);
+
+let options = {
+    filter: {
+        value: [],
+    },
+    fromBlock: 0
+};
+
+//spiegone @ https://www.coinclarified.com/p/3-ways-to-subscribe-to-events-with-web3-js/
+ShopContract.events.addedPaymentEntry(options)
+    .on('data', (event: any) => console.log(event)) //evento ricevuto
+    .on('error', (err: any) => console.log(err))
+    .on('connected', (str: any) => console.log(str));
+
--- server/routes/PoCRounter.ts
+++ server/routes/PoCRounter.ts
@@ -25,12 +25,15 @@ PoCRounter.get("/", async (req: Request, res: Response) => {
 PoCRounter.get("/qr", async (req: Request, res: Response) => {
     /*
        TL;DR
-       1) Metamask su android non permette di collegarsi direttamente a un server locale senza passare per un DNS
-       2) sslip.io (perchè quello indicato sulla loro documentazione ufficiale neanche esiste più da un bel po') ci fa da DNS
-       3) Ora i deep link funzionano solo se in HTTPS, Android dalla versione 7 non permette di aggiungere certificati senza ricompilare l'app
-       4) Creiamo un link HTTPS a tinyurl, che punta a sslip.io con nostro server locale in HTTP, i.e: http://192.168.0.15.sslip.io:8080/PoC/transazione
-       5) Notare come tinyurl è uno dei pochi shortener che permette parametri in GET
-       6) Creiamo il nostro bel QR così finchè non avremo un server HTTPS in produzione con dei certificati veri
+       1) Metamask su android non permette di collegarsi direttamente a un server locale su Android in quanto l'HTTP non è permesso (CLEAR_TEXT_NOT_PERMITTED)
+       2) Metamask ha un eccezione hardcoddata che lo permette per le connessioni provenienti da sslip.io (la documentazione dice di usare xip.io che non è sbagliato da agosto)
+       3) PR per aggiornare la documentazione: https://github.com/MetaMask/metamask-docs/pull/396
+       4) I deep link funzionano solo se in HTTPS, Android dalla versione 7 non permette di aggiungere certificati senza ricompilare l'app
+       5) Creiamo un link HTTPS a tinyurl, che punta a sslip.io con nostro server locale in HTTP, i.e: http://192.168.0.15.sslip.io:8080/PoC/transazione
+       6) Notare come tinyurl è uno dei pochi shortener che permette parametri in GET
+       7) Creiamo il nostro bel QR così finchè non avremo un server HTTPS in produzione con dei certificati veri
+       8) TODO: investigare l'utilizzo di "ngrok" per un tunneling HTTPS, ma probabilemente non ne vale la pena
+       9) In produzione sarà compito dei sistemisti configurare il traffico HTTP in uscita dal server per essere in HTTPS
     */
 
     const qr_str = "https://metamask.app.link/dapp/" + "tinyurl.com/2p9xf7wx" + "?id=" + req.query.id;
