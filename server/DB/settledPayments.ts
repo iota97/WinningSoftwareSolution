dd138bca10bb83fb7741cda2c5be2e3bab6ecd2b
iota97 <giovi.cocco@gmail.com>
2022-02-07 14:31:15 +0100

    Few test

--- server/DB/settledPayments.ts
+++ server/DB/settledPayments.ts
@@ -4,36 +4,39 @@ import { OkPacket, RowDataPacket } from "mysql2";
 
 export const create = (pagamento: settledPayment, callback: Function) => {
   const queryString = "INSERT IGNORE INTO SettledPayment (id, acquirente) VALUES (?, ?)"
-
+  
   db.query(
     queryString,
     [pagamento.id, pagamento.acquirente],
     (err, result) => {
-      if (err) {callback(err)};
-
+      if (err) {
+        callback(err)
+        return;
+      }
+      
       const insertId = (<OkPacket> result).insertId;
       callback(null, insertId);
     }
-  );
-};
-
-export const findAll = (callback: Function) => {
-  const queryString = `SELECT * FROM SettledPayment`
-
-  db.query(queryString, (err, result) => {
-    if (err) {callback(err)}
-
-    const rows = <RowDataPacket[]> result;
-    const pagamenti: settledPayment[] = [];
-
-    rows.forEach(row => {
-      const pagamento: settledPayment =  {
-	id: row.id,
-        acquirente: row.acquirente,
-      }
-
-      pagamenti.push(pagamento);
+    );
+  };
+  
+  export const findAll = (callback: Function) => {
+    const queryString = `SELECT * FROM SettledPayment`
+    
+    db.query(queryString, (err, result) => {
+      if (err) {callback(err)}
+      
+      const rows = <RowDataPacket[]> result;
+      const pagamenti: settledPayment[] = [];
+      
+      rows.forEach(row => {
+        const pagamento: settledPayment =  {
+          id: row.id,
+          acquirente: row.acquirente,
+        }
+        
+        pagamenti.push(pagamento);
+      });
+      callback(null, pagamenti);
     });
-    callback(null, pagamenti);
-  });
-}
+  }
--- server/__tests__/main.ts
+++ server/__tests__/main.ts
@@ -1,17 +1,67 @@
 import request from 'supertest'
-import {Express} from 'express-serve-static-core'
-
+import {paymentEntry} from "../DB/types/paymentEntry";
+import {settledPayment} from "../DB/types/settledPayment";
+import * as paymentEntryModel from "../DB/paymentEntries";
+import * as settledPaymentModel from "../DB/settledPayments";
 import {createWebServer} from '../routes/index'
+import {initDB} from "../DB/initDB";
+
+initDB();
 
 describe('Server', function () {
-  let server : Express
-  beforeEach(function () {
-    server = createWebServer();
-  });
-
-  it('Connect to /', function testSlash(done) {
-  request(server)
+  const server = createWebServer();
+  
+  it('Connect to server', function testSlash(done) {
+    request(server)
     .get('/')
     .expect(200, done)
+  });
+  
+  it('QR', function testSlash(done) {
+    request(server)
+    .get('/qr?id=0')
+    .expect(200, done)
+  });
+  
+  it('Transazioni', function testSlash(done) {
+    request(server)
+    .get('/transazioni')
+    .expect(200, done)
+  });
+  
+  it('Transazione', function testSlash(done) {
+    request(server)
+    .get('/transazione?id=0')
+    .expect(200, done)
+  });
+  
+  it('Transazione', function testSlash(done) {
+    request(server)
+    .get('/items')
+    .expect(200, done)
+  });
+});
+
+describe('DB', function () {
+  it('Duplicate key on paymentEntry', function testSlash(done) {
+    const pagamento: paymentEntry =  {
+      id: "0",
+      ecommerce: "asdf",
+    }
+    paymentEntryModel.create(pagamento, (res: any) => {
+      expect(res == "Error: Duplicate entry '0' for key 'PRIMARY'")
+      done()
+    });
+  });
+  
+  it('Duplicate key on settledPayment', function testSlash(done) {
+    const pagamento: settledPayment =  {
+      id: "0",
+      acquirente: "asdf",
+    }
+    settledPaymentModel.create(pagamento, (res: any) => {
+      expect(res == "Error: Duplicate entry '0' for key 'PRIMARY'")
+      done()
+    });
   });
 });
