b8328584631772c6226bbe152d6004a366e64088
galvagnimatteo <galvagni.matteo@protonmail.com>
2022-02-26 14:54:31 +0100

    sistemato parzialmente scambio a stable

--- contract/contracts/ShopContract.sol
+++ contract/contracts/ShopContract.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.12;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -11,21 +10,28 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
 contract ShopContract is Ownable, KeeperCompatibleInterface {
+
     using Address for address;
-    using SafeMath for uint256;
 
     uint256 freePaymentEntryId;
     uint256 freeSettledPaymentId;
-    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal priceFeedMatic;
+    AggregatorV3Interface internal priceFeedDAI;
 
     uint256 immutable interval;
     uint256 immutable expireDuration;
     uint256 lastTimeStamp;
     uint256 lastTimeIndex;
 
+    uint256 slippageClient; //in x1000, so 5 -> 0.5% slippage
+
     IUniswapV2Router02 public immutable uniswapV2Router;
+    IUniswapV2Factory public immutable uniswapV2Factory;
+
     IERC20 DAI = IERC20(0xcB1e72786A6eb3b44C2a2429e317c8a2462CFeb1);
 
     struct PaymentEntry {
@@ -50,9 +56,10 @@ contract ShopContract is Ownable, KeeperCompatibleInterface {
      * Aggregator: MATIC/USD Dec: 8
      * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
      */
-    constructor(uint256 _interval, uint256 _expireDuration){
+    constructor(uint256 _interval, uint256 _expireDuration, uint256 _slippageClient){
 
-        priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
+        priceFeedMatic = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
+        priceFeedDAI = AggregatorV3Interface(0x0FCAa9c899EC5A91eBc3D5Dd869De833b06fB046);
 
         freePaymentEntryId = 0;
         freeSettledPaymentId = 0;
@@ -60,21 +67,23 @@ contract ShopContract is Ownable, KeeperCompatibleInterface {
         lastTimeStamp = block.timestamp;
         lastTimeIndex = 0;
 
-        interval = _interval; // Check every 60 sec, change to 1 hour
-        expireDuration = _expireDuration; // expire after 10 min, change to 14 days
+        interval = _interval;
+        expireDuration = _expireDuration;
 
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x8954AfA98594b838bda56FE4C12a09D7739D179b); //uniswapv2 router for quickswap mumbai
-        uniswapV2Router = _uniswapV2Router;
+        slippageClient = _slippageClient;
+
+        uniswapV2Router = IUniswapV2Router02(0x8954AfA98594b838bda56FE4C12a09D7739D179b); //uniswapv2 router for quickswap mumbai
+        uniswapV2Factory = IUniswapV2Factory(uniswapV2Router.factory());
 
     }
 
     event addedPaymentEntry(uint256 paymentEntryId);
     event paymentSettled(uint256 settledPaymentId);
     event statusChanged(uint256 settledPaymentId);
+    event test(uint256 value);
 
-    function getLatestPrice() public view returns (uint256 p) {
-        (,int price,,,) = priceFeed.latestRoundData();
-        return (10**24)/uint256(price);
+    function getSlippageAmount(uint256 number) public returns(uint256){
+        return (number * slippageClient)/1000;
     }
 
     function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
@@ -114,9 +123,10 @@ contract ShopContract is Ownable, KeeperCompatibleInterface {
 
         require(paymentEntryId < freePaymentEntryId);
 
-        uint256 valueWithSlippage = msg.value.add(msg.value.div(200)); //0.5% slippage
+        uint256 valueWithSlippage = msg.value + getSlippageAmount(msg.value);
 
-        uint256 priceInMatic = paymentsEntries[paymentEntryId].price * getLatestPrice();
+        (,int priceMatic,,,) = priceFeedMatic.latestRoundData(); //8 decimals
+        uint256 priceInMatic = paymentsEntries[paymentEntryId].price * (10**24)/uint256(priceMatic);
 
         require(valueWithSlippage >= priceInMatic); //assure that you can pay with slippage
         //TODO fare in modo che uno non possa pagare un prezzo molto più alto del dovuto, non è necessario ma è una feature carina se no uno potrebbe perdere molti soldi
@@ -126,10 +136,13 @@ contract ShopContract is Ownable, KeeperCompatibleInterface {
         address[] memory path = new address[](2);
         path[0] = uniswapV2Router.WETH(); //WMATIC canonic address
         path[1] = address(DAI);
+/*
+        (,int priceDAI,,,) = priceFeedDAI.latestRoundData(); //8 decimals
+        uint256 minAmountDAI = paymentsEntries[paymentEntryId].price * (10**24)/uint256(priceDAI); //price is in USD cents, adapt 8 decimals priceDAI to 18 decimals DAI
+*/
+        uint[] memory amountsDAI = uniswapV2Router.swapExactETHForTokens{value: msg.value}(0, path, address(this), block.timestamp); //EXACT amount of DAI received
 
-        uint256 minAmountDai = 0; //TODO IMPORTANTE: assicurarsi che questa variabile sia il minimo di DAI (incluso slippage) che siamo disposti ad accettare per lo scambio, in caso contrario contratto vulnerabile a exploit
-                                  //si potrebbe assumere 1 DAI = 1 USD ma fluttua anche se di pochi centesimi, forse meglio usare chainlink?
-        uint[] memory amountsDAI = uniswapV2Router.swapExactETHForTokens{value: msg.value}(minAmountDai, path, address(this), block.timestamp); //EXACT amount of DAI received
+        //uniswap returns a much lower number of DAI, maybe this is a problem only on ganache? 
 
         settledPayments[freeSettledPaymentId] = SettledPayment(paymentEntryId, 1, msg.sender, block.timestamp, amountsDAI[amountsDAI.length - 1]);
         freeSettledPaymentId = freeSettledPaymentId + 1;
